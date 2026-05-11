7fc4485c656ef6cb3164aeed7b0bf58998f4ee80
Ben Dean <ben@yeet.cx>
2026-05-11 10:04:04 -0500

    update nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "srcery: the single nixpkgs pin for yeet-src";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
